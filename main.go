cf3cd05cbae69bbfee3fba908c8482bdbc23c5b3
magodo <wztdyl@sina.com>
2024-08-05 14:54:30 +0800

    profile

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"flag"
 	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/Azure/terraform-provider-azapi/internal/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 	// remove date and time stamp from log output as the plugin SDK already adds its own
@@ -18,6 +24,18 @@ func main() {
 	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	serveOpts := providerserver.ServeOpts{
 		Debug:   debugMode,
 		Address: "registry.terraform.io/Azure/azapi",
@@ -27,5 +45,17 @@ func main() {
 
 	if err != nil {
 		log.Fatalf("Error serving provider: %s", err)
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
 	}
 }
