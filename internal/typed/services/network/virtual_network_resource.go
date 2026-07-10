6087327a57fad68fa35d9b4bbec5d165cec5f7df
magodo <wztwcy@gmail.com>
2026-07-10 18:57:28 +1000

    Try schema hook

--- internal/typed/services/network/virtual_network_resource.go
+++ internal/typed/services/network/virtual_network_resource.go
@@ -2,13 +2,23 @@ package network
 
 import (
 	"github.com/Azure/terraform-provider-azapi/internal/typed/servicehooks"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
 func NewAzApiVirtualNetworkResource() AzApiVirtualNetworkResource {
 	return AzApiVirtualNetworkResource{
 		hooks: servicehooks.ResourceHooks{
 			SchemaHook: func(s schema.Schema) schema.Schema {
+				addressSpace := s.Attributes["properties"].(schema.SingleNestedAttribute).Attributes["address_space"].(schema.SingleNestedAttribute)
+
+				addressPrefixes := addressSpace.Attributes["address_prefixes"].(schema.SetAttribute)
+				addressPrefixes.Validators = []validator.Set{
+					setvalidator.SizeAtLeast(1),
+				}
+				addressSpace.Attributes["address_prefixes"] = addressPrefixes
+
 				return s
 			},
 		},
